7625b2661c02c704b2731d048271bbb005bb789b
gayathri1299 <prerepag@gmail.com>
2020-03-14 14:29:37 +0530

    plot4 commit

---
+++ plot4.R
@@ -0,0 +1,6 @@
+chart <- ggplot(baltYrTypEmm, aes(year, Emissions, color = type))
+chart <- chart + geom_line() +
+  xlab("year") +
+  ylab(expression('Total Emissions')) +
+  ggtitle('Total Baltimore Emissions [2.5]* From 1999 to 2008')
+print(chart)
