55ed15b523c7e26c64d2ffdfbe807680363b582a
gayathri1299 <prerepag@gmail.com>
2020-03-14 14:31:46 +0530

    plot4 correction

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
-chart <- ggplot(baltYrTypEmm, aes(year, Emissions, color = type))
-chart <- chart + geom_line() +
-  xlab("year") +
+chart1 <- ggplot(baltYrTypEmm, aes(factor(year), Emissions))
+chart1 <- chart + geom_bar(stat="identity") +
+  xlab("year") +  
   ylab(expression('Total Emissions')) +
-  ggtitle('Total Baltimore Emissions [2.5]* From 1999 to 2008')
-print(chart)
+  ggtitle('Total [2.5]* Coal Emissions From 1999 to 2008')
+print(chart1)
