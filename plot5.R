bb391bd128f6c41ab739fe5acdf572b21228038c
gayathri1299 <prerepag@gmail.com>
2020-03-14 14:59:05 +0530

    plot6 commit

--- plot5.R
+++ plot5.R
@@ -4,10 +4,3 @@ chart <- chart + geom_bar(stat="identity") +
   ylab(expression('Total Emissions')) +
   ggtitle('Baltimore Motor Vehicle PM[2.5] Emissions From 1999 to 2008')
 print(chart)
-baltYrTypEmmFips <- summarise(group_by(filter(NEI_data, NEI_data$fips == "24510"& type == 'ON-ROAD'), year), Emissions=sum(Emissions))
-laYrTypEmmFips <- summarise(group_by(filter(NEI_data, NEI_data$fips == "06037"& type == 'ON-ROAD'), year), Emissions=sum(Emissions))
-
-baltYrTypEmmFips$County <- "Baltimore City, MD"
-laYrTypEmmFips$County <- "Los Angeles County, CA"
-
-baltLaEmissions <- rbind(baltYrTypEmmFips, laYrTypEmmFips)
